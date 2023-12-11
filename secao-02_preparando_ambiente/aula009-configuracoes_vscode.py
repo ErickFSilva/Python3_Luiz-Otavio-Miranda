6a9bbae99dc7a2588fb6bf8ec9bdd7614e3be519
ErickFSilva <erick_ferreira@outlook.com>
2023-12-11 15:28:19 -0300

    Seção-02 preparando o ambiente, 11/11/2023 (tarde)

---
+++ secao-02_preparando_ambiente/aula009-configuracoes_vscode.py
@@ -0,0 +1,10 @@
+# (alt+shift+seta p/ baixo): copia a linha
+
+# Qualquer texto
+# Qualquer texto
+# Qualquer texto
+
+
+# Imprime em tela
+print('Hello, world!')
+print('Opa, editei...')
---
+++ secao-02_preparando_ambiente/aula012-configuracoes-python-vscode-linux.py
@@ -0,0 +1,21 @@
+# - Configurações adicionais do arquivo: settings.json
+
+# {
+#     "workbench.startupEditor": "none",
+#     "explorer.confirmDragAndDrop": false,
+#     "explorer.confirmDelete": false,
+#     "editor.minimap.enabled": false,
+#     "git.autofetch": true,
+#     "git.confirmSync": false,
+#     "files.autoGuessEncoding": true,
+#     "window.menuBarVisibility": "compact",
+#     "explorer.compactFolders": false,
+#     "code-runner.runInTerminal": true,
+#     "code-runner.executorMap": {
+#         "javascript": "node",
+#         "python": "clear ; python -u"
+#     },
+#     "code-runner.ignoreSelection": true,
+#     "workbench.iconTheme": "material-icon-theme",
+#     "python.defaultInterpreterPath": "python"
+# }
