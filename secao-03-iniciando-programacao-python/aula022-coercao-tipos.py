5a8e963f05cfb18e8d5198145dcf6ab85e04aad0
ErickFSilva <erick_ferreira@outlook.com>
2023-12-29 17:12:49 -0300

    Seção-03. Iniciando na programação com python, 27/12/2023 (tarde)

---
+++ secao-03-iniciando-programacao-python/aula022-coercao-tipos.py
@@ -0,0 +1,13 @@
+"""
+- Conversão de tipos, coerção;
+- Type convertion, typecasting, coercion: é o ato de converter um tipo em outro;
+- Tipos imutáveis e primitivos: str, int, float, bool;
+"""
+
+print(type('1'))
+print(type(int('1')))
+
+print(int('1') + 1)
+print(float('1') + 1)
+print(bool('1'), bool(''), bool(1))
+print(str(11) + 'b')
---
+++ secao-03-iniciando-programacao-python/aula023-introducao-variaveis-python.py
@@ -0,0 +1,15 @@
+# Variáveis são usadas para salvar algo na memória do computador;
+# PEP8: inicie variáveis com letras minúsculas, pode usar números e _ (underline);
+# O sinal de = (igual) é operador de atribuição. Ele é usado para atribuir um valor uma variável;
+# Uso: nome_variavel = expressão;
+
+nome = "Erick"
+sobrenome = "Ferreira da Silva"
+idade = 17
+maior_de_idade = idade >= 18
+soma_dois_valores = 2 + 2
+int_um = int('1')
+
+print(int_um, type(int_um))
+print('Nome: ', nome, '; Idade:', idade)
+print('É maior? ', maior_de_idade)
